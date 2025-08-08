09f503109b83f880cdae6408031706de1eb56926
sabrikayraatalay <sabrikayraatalay@localhost>
2025-08-08 18:53:17 +0300

    address transactions done

--- gallerist_project/src/main/java/com/KayraAtalay/controller/impl/RestAddressControllerImpl.java
+++ gallerist_project/src/main/java/com/KayraAtalay/controller/impl/RestAddressControllerImpl.java
@@ -1,6 +1,10 @@
 package com.KayraAtalay.controller.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +16,8 @@ import com.KayraAtalay.controller.RootEntity;
 import com.KayraAtalay.dto.DtoAddress;
 import com.KayraAtalay.dto.DtoAddressIU;
 import com.KayraAtalay.service.IAddressService;
+import com.KayraAtalay.utils.PageableRequest;
+import com.KayraAtalay.utils.RestPageableEntity;
 
 import jakarta.validation.Valid;
 
@@ -26,6 +32,27 @@ public class RestAddressControllerImpl extends RestBaseController implements IRe
 	@Override
 	public RootEntity<DtoAddress> saveAddress(@Valid @RequestBody DtoAddressIU dtoAddressIU) {
 		return ok(addressService.saveAddress(dtoAddressIU));
+	}
+
+	@GetMapping("/{id}")
+	@Override
+	public RootEntity<DtoAddress> findAddressById(@PathVariable Long id) {
+		return ok(addressService.findAddressById(id));
+	}
+
+	@GetMapping("/list/pageable")
+	@Override
+	public RootEntity<RestPageableEntity<DtoAddress>> findAllPageable(PageableRequest pageable) {
+		
+		Page<DtoAddress> page = addressService.findAllPageableDto(toPageable(pageable));
+		RestPageableEntity<DtoAddress> pageableResponse = toPageableResponse(page, page.getContent());
+		return ok(pageableResponse);
+	}
+
+	@DeleteMapping("/delete/{id}")
+	@Override
+	public RootEntity<DtoAddress> deleteAddressById(@PathVariable Long id) {
+		return ok(addressService.deleteAddressById(id));
 	}
 
 }
--- gallerist_project/src/main/java/com/KayraAtalay/service/impl/AddressServiceImpl.java
+++ gallerist_project/src/main/java/com/KayraAtalay/service/impl/AddressServiceImpl.java
@@ -1,13 +1,21 @@
 package com.KayraAtalay.service.impl;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.KayraAtalay.dto.DtoAddress;
 import com.KayraAtalay.dto.DtoAddressIU;
+import com.KayraAtalay.exception.BaseException;
+import com.KayraAtalay.exception.ErrorMessage;
+import com.KayraAtalay.exception.MessageType;
 import com.KayraAtalay.model.Address;
 import com.KayraAtalay.repository.AddressRepository;
 import com.KayraAtalay.service.IAddressService;
@@ -26,6 +34,14 @@ public class AddressServiceImpl implements IAddressService{
 		
 		return address;
 	}
+	
+	private DtoAddress toDto(Address address) {
+		DtoAddress dtoAddress = new DtoAddress();
+		
+		BeanUtils.copyProperties(address, dtoAddress);
+		
+		return dtoAddress;
+	}
 
 	@Override
 	public DtoAddress saveAddress(DtoAddressIU dtoAddressIU) {
@@ -34,6 +50,52 @@ public class AddressServiceImpl implements IAddressService{
 		
 		BeanUtils.copyProperties(savedAddress, dtoAddress);
 		return dtoAddress;
+	}
+
+	@Override
+	public Page<Address> findAllPageable(Pageable pageable) {
+		Page<Address> page = addressRepository.findAll(pageable);
+		return page;
+	}
+
+	@Override
+	public Page<DtoAddress> findAllPageableDto(Pageable pageable) {
+		Page<Address> addressPage = addressRepository.findAll(pageable);
+
+	    if (addressPage.isEmpty()) {
+	        throw new BaseException(new ErrorMessage(MessageType.NO_RECORD_EXIST, null));
+	    }
+
+	    List<DtoAddress> dtoAddresses = new ArrayList<>();
+	    for (Address address : addressPage.getContent()) {
+	        dtoAddresses.add(toDto(address));
+	    }
+
+	    return new org.springframework.data.domain.PageImpl<>(
+	        dtoAddresses, 
+	        pageable, 
+	        addressPage.getTotalElements()
+	    );
+	}
+
+	@Override
+	public DtoAddress findAddressById(Long id) {
+		Optional<Address> optAddress = addressRepository.findById(id);
+		if (optAddress.isEmpty()) {
+			throw new BaseException(new ErrorMessage(MessageType.ADDRESS_NOT_FOUND, id.toString()));
+		}
+		
+		return toDto(optAddress.get());
+	}
+
+	@Override
+	public DtoAddress deleteAddressById(Long id) {
+		Optional<Address> optAddress = addressRepository.findById(id);
+		if (optAddress.isEmpty()) {
+			throw new BaseException(new ErrorMessage(MessageType.ADDRESS_NOT_FOUND, id.toString()));
+		}
+		addressRepository.delete(optAddress.get());
+		return toDto(optAddress.get());
 	}
 
 }
